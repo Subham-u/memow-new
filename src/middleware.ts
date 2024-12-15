68e01aff37d5e4c64e3ff34ff24e95044000fbea
Subham-u <subhammahapatra004@gmail.com>
2024-12-16 04:16:58 +0530

    auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-
-const isPublicRoute = createRouteMatcher([
-    "/sign-in",
-    "/sign-up",
-    "/",
-])
-const isPublicApiRoute = createRouteMatcher([
-    "/api/videos",
-    "/api/webhooks/clerk",
-])
-
-
-export default clerkMiddleware(async (auth, req) => {
-    
-    const { userId } = await auth();
-    console.log(userId);
-    const currentUrl = new URL(req.url)
-     const isAccessingDashboard = currentUrl.pathname === "/"
-     const isApiRequest = currentUrl.pathname.startsWith("/api")
-
-     // If user is logged in and accessing a public route but not the dashboard
-    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
-        return NextResponse.redirect(new URL("/", req.url))
-    }
-    //not logged in
-    if(!userId){
-        // If user is not logged in and trying to access a protected route
-        if(!isPublicRoute(req) && !isPublicApiRoute(req) ){
-            return NextResponse.redirect(new URL("/sign-up", req.url))
-        }
-
-        // If the request is for a protected API and the user is not logged in
-        if(isApiRequest && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/sign-up", req.url))
-        }
-    }
-    return NextResponse.next()
-
-})
-
+import { clerkMiddleware } from '@clerk/nextjs/server'
+ 
+export default clerkMiddleware({
+//   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
